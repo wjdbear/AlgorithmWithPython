bfb1aecdda9cc9d77365a55f1ed9d7b284fdc3a0
wjdbear <75930140+wjdbear@users.noreply.github.com>
2021-02-06 01:23:09 +0900

    Create 4834.py

---
+++ Programming-Intermediate/List1/4834.py
@@ -0,0 +1,21 @@
+t = int(input())
+
+for i in range(t):
+	n = int(input())
+	numCard = input()
+	cardList = []
+	for j in numCard:
+		cardList.append(j)
+	orgCardList = list(set(cardList))
+	
+	maxNum = 0
+	
+	for k in range(len(orgCardList)):
+		result = cardList.count(orgCardList[k])
+		if maxNum < result:
+			maxNum = result
+			number = orgCardList[k]
+		if maxNum == result:
+			if number
+			
+	print("#%s %s %s" %(i+1,number,maxNum))
