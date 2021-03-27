7465de5226a4fbe728bcb9023a4e10c257e511fc
wjdbear <75930140+wjdbear@users.noreply.github.com>
2021-03-27 19:33:23 +0900

    Update 1157.py

--- solved_ac/bronze/1157.py
+++ solved_ac/bronze/1157.py
@@ -1,1 +1,15 @@
+words = input().upper()
+org_words = list(set(words))
 
+count_list = [] 
+
+for i in org_words:
+    count = words.count(i)
+    count_list.append(count) # [4,1,1,4]
+
+if count_list.count(max(count_list)) > 1:
+    print("?")
+    
+else:
+    loc = count_list.index(max(count_list))
+    print(org_words[loc])
