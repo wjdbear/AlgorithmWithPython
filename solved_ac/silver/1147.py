21b2d297683a986b3979dffadb7fd36ea68b44e1
wjdbear <75930140+wjdbear@users.noreply.github.com>
2021-03-30 02:56:10 +0900

    Update 1147.py

--- solved_ac/silver/1147.py
+++ solved_ac/silver/1147.py
@@ -1,1 +1,20 @@
+candidate = int(input())
+canList = []
 
+for i in range(candidate):
+    canList.append(int(input()))
+
+dasom = canList[0]
+otherCand = canList[1:-1]
+
+if candidate == 1:
+    print(0)
+else:
+    result = 0
+    otherCand = sorted(otherCand, reverse=True)
+    while otherCand[0] >= dasom:
+        dasom += 1
+        otherCand[0] -= 1
+        result += 1
+        otherCand = sorted(otherCand, reverse=True)
+    print(result)
